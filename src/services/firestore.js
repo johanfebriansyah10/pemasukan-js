5baedd2239946a4a3a05bdb9829107fdea64d09b
Johanfebriansyah <johanfebriansyah2022@gmail.com>
2024-12-06 21:06:06 +0800

    aman

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,11 +1,11 @@
 const admin = require('firebase-admin');
 
 
-const serviceAccount = require('../../service-account/soy-beaker-440202-j7-e03a3b850d9d.json');
+const serviceAccount = require('../../namafileserviceaccount');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://soy-beaker-440202-j7.firebaseio.com",
+    databaseURL: "https://projectid.firebaseio.com",
 });
 
 const db = admin.firestore();
